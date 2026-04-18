a9086bacaa882d2bb2575a3a6d4d07135887ff59
Atharva Ranjane <atharva@Atharvas-MacBook-Pro.local>
2026-04-18 17:24:35 +0200

    Initial landing showcase

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+
+export type Page = 'chat' | 'library' | 'graph' | 'compare'
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/"    element={<LandingPage />} />
+        <Route path="/app" element={<LandingPage />} />
+        <Route path="*"    element={<Navigate to="/" replace />} />
+      </Routes>
+    </HashRouter>
+  )
+}
